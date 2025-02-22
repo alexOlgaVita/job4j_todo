d4c680cc04a5d8c2808c7acf4d2f285cbd220c01
olga.ilyina <olga.ilyina@axenix.pro>
2025-02-22 20:16:36 +0300

    4. TODO list [#3786 #533548]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -22,7 +22,7 @@ public class Main {
          * Проблема возникала при выполнении запросов: "java.lang.ClassCastException: class ru.job4j.model.Task cannot
          *  be cast to class ru.job4j.model.Task (ru.job4j.model.Task is in unnamed module of loader 'app';
          * ru.job4j.model.Task is in unnamed module of loader org.springframework.boot.devtools.restart.classloader.RestartClassLoader @853c3ea)"
-         * источкник: https://forum.jmix.ru/t/podklyuchenie-spring-boot-devtools/112?ysclid=m7dm9o7xh6819232596
+         * источкник: "https://forum.jmix.ru/t/podklyuchenie-spring-boot-devtools/112?ysclid=m7dm9o7xh6819232596"
          * */
         System.setProperty("spring.devtools.restart.enabled", "false");
 
